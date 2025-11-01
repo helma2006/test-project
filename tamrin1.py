8782c8d31cdd9651f5ca95d92b5f303c7a6af847
helma2006 <helmashahbazi2005@gmail.com>
2025-11-01 12:00:23 +0330

    crated tamrin1.py
    this is first file

---
+++ tamrin1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name please: ")
+birth_year = int(input("Enter your birth year please: "))
+age = 2025 - birth_year
+print(f"Hello, {name}! You are approximately {age} years old :)")
